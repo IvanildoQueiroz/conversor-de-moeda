aa20902c7cdc20ad988fc742bac46fd8dda3dd23
Ivanildo Queiroz <ivanildo.jua@hotmail.com>
2024-04-18 02:10:39 -0300

    nova pagina adicionada

---
+++ js/indice.js
@@ -0,0 +1,43 @@
+const controls = document.querySelectorAll(`.control`);
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+
+controls.forEach((control) => {
+  control.addEventListener("click", () => {
+    const isLeft = control.classList.contains("arrow-left");
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+    items.forEach((item) => {
+      item.classList.remove("current-item");
+      items[currentItem].scrollIntoView({
+        behavior: "smooth",
+        inline: "center",
+        
+      });
+      items[currentItem].classList.add("current-item");
+    });
+  });
+  let newItems = [];
+  items.forEach((item) => {
+    newItems.push(item);
+    item.addEventListener("click", (e) => {
+      newItems.filter((e, i) => {
+        if (e != item) e.classList.remove("current-item");
+        if (e == item) currentItem = i;
+      });
+      // currentItem = i;
+      e.target.parentElement.classList.add("current-item");
+    });
+  });
+});
