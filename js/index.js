4b20b2f3145bce31796abc75e9f2681065eaf75c
Ivanildo Dos Santos Queiroz <isqueiroz@br-atacadao.corp>
2024-04-29 15:11:06 -0300

    chart customization

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
-
 const secondTypeCoin = document.querySelector("#toConversion");
 
 secondTypeCoin.addEventListener("change", (e) => {
   e.preventDefault();
-  const selectedValeu = document.querySelector('#selectedValue');
+  const selectedValeu = document.querySelector("#selectedValue");
   const descriptionCoin = document.querySelector(".descriptionCoin");
   const descriptionCoin2 = document.querySelector("#descriptionCoin2");
   descriptionCoin.textContent = secondTypeCoin.value;
   descriptionCoin2.textContent = secondTypeCoin.value;
   selectedValeu.textContent = secondTypeCoin.value;
-  getCoin(secondTypeCoin.value)
+  getCoin(secondTypeCoin.value);
 });
 
 function loadNameCoin() {
@@ -40,7 +39,7 @@ function createOptionsCoin(coin, description) {
 
 function conversionCoin(coin) {
   for (const key in coin) {
-    calcConversion(coin[key].high)
+    calcConversion(coin[key].high);
   }
 
 }
@@ -50,100 +49,124 @@ function getCoin(coin) {
     .then((json) => conversionCoin(json));
 }
 
-
 function calcConversion(VT) {
-  const Va = document.querySelector('#valueCoin_1');
-  const Vb = document.querySelector('#valueCoin_2');
+  const Va = document.querySelector("#valueCoin_1");
+  const Vb = document.querySelector("#valueCoin_2");
 
-  
-  Vb.value = ((Va.value / VT) / 1).toFixed(2);
-  
-  Va.addEventListener('input', () => {
+  Vb.value = (Va.value / VT / 1).toFixed(2);
+
+  Va.addEventListener("input", () => {
     Vb.value = (Va.value / VT).toFixed(2);
   });
-  Vb.addEventListener('input', () => {
+  Vb.addEventListener("input", () => {
     Va.value = (Vb.value * VT).toFixed(2);
   });
-  console.log(Va.value, Vb.value);
-  alterDataDescription(Va.value,Vb.value);
+
+  alterDataDescription(Va.value, Vb.value);
 }
 loadNameCoin();
 
 function insertClass() {
-  const timeList = document.querySelector('#tabList');
-  const timeListAll = timeList.querySelectorAll('li');
+  const timeList = document.querySelector("#tabList");
+  const timeListAll = timeList.querySelectorAll("li");
 
   timeListAll.forEach((e) => {
-    e.addEventListener('click', function () {
-      timeListAll.forEach(item=>{
-        if(item!==e)item.classList.remove('active');
-      })
-      e.classList.toggle('active')
+    e.addEventListener("click", function () {
+      timeListAll.forEach((item) => {
+        if (item !== e) item.classList.remove("active");
+      });
+      e.classList.toggle("active");
     });
-    
-  })
+  });
 }
 insertClass();
 
-function calcTimeStamp(x){
-  let today = new Date(1714165120 *1000);
-  const date = new Date(x*1000);
-  today.setHours(today.getHours()-3)
-  console.log(today.toLocaleString('pt-br', {timeZone: 'UTC',dateStyle:'short'}));
-  return date.toLocaleString('pt-br', {timeZone: 'UTC',dateStyle:'short'})
+function calcTimeStamp(x) {
+  let today = new Date(1714165120 * 1000);
+  const date = new Date(x * 1000);
+  today.setHours(today.getHours() - 3);
+  return date.toLocaleString("pt-br", { timeZone: "UTC", dateStyle: "short" });
 }
-calcTimeStamp()
+calcTimeStamp();
 
- function alterDataDescription(x,y){
-  let calc = x/y;
-  document.querySelector(`#panel2`).innerHTML=`${(calc*10).toFixed(2)} reais`
-  document.querySelector(`#panel3`).innerHTML=`${(calc*100).toFixed(2)} reais`
-  document.querySelector(`#panel4`).innerHTML=`${(calc*1000).toFixed(2)} reais`
-  document.querySelector(`#panel5`).innerHTML=`${(calc*10000).toFixed(2)} reais`
- }
+function alterDataDescription(x, y) {
+  let calc = x / y;
+  document.querySelector(`#panel2`).innerHTML = `${(calc * 10).toFixed(
+    2
+  )} reais`;
+  document.querySelector(`#panel3`).innerHTML = `${(calc * 100).toFixed(
+    2
+  )} reais`;
+  document.querySelector(`#panel4`).innerHTML = `${(calc * 1000).toFixed(
+    2
+  )} reais`;
+  document.querySelector(`#panel5`).innerHTML = `${(calc * 10000).toFixed(
+    2
+  )} reais`;
+}
 // chart
-async function showChart(){
-  async function getSpecificPeriod(x,y){
-    let urlApi = `https://economia.awesomeapi.com.br/json/daily/USD-BRL/10?start_date=${x}&end_date=${y}`;
-    try{
-      
-      const res = await fetch(urlApi);
-      const data = await res.json();
-      // console.log(data)
-      return data
+secondTypeCoin.addEventListener("change", (e) => {
+  async function showChart(coin) {
+    async function getSpecificPeriod(x, y) {
+      let data, data_BRL;
+      let urlApi_BRL = `https://economia.awesomeapi.com.br/json/daily/BRL-${coin}/10?start_date=${x}&end_date=${y}`;
+      let urlApi = `https://economia.awesomeapi.com.br/json/daily/${coin}-BRL/10?start_date=${x}&end_date=${y}`;
+      try {
+        const res_BRL = await fetch(urlApi_BRL);
+        data_BRL = await res_BRL.json();
 
-    }catch(e){
-
-      console.log(e)
-      return null
-    }
-
-  }
-
-  //set initial data com 20240425 =  25/04/2024;
-  
-  const monthVariation = await getSpecificPeriod(20230102,20230123);
-  const ctx = document.getElementById('chart');
-  console.log(monthVariation)
-
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: monthVariation.map(i=>calcTimeStamp(i.timestamp)),
-      datasets: [{
-        label: 'high value',
-        data: monthVariation.map(i => i.high),
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: false
-        }
+        const res = await fetch(urlApi);
+        data = await res.json();
+        // console.log(data)
+        return { data, data_BRL };
+      } catch (e) {
+        console.log(e);
+        return null;
       }
     }
-  });
-}
-showChart();
- 
+
+    //set initial data com 20240425 =  25/04/2024;
+    const dataApi = await getSpecificPeriod(20230102, 20230123);
+    const monthVariation_BRL = dataApi.data_BRL;
+    const monthVariation = dataApi.data;
+
+    const ctx = document.getElementById("chart");
+
+    new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: monthVariation.map((i) => calcTimeStamp(i.timestamp)),
+        datasets: [
+          {
+            label: `high ${coin}`,
+            data: monthVariation.map((i) => i.high),
+            borderWidth: 1,
+          },
+          {
+            label: "high BRL",
+            data: monthVariation_BRL.map((i) => i.high),
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: false,
+          },
+        },
+        responsive: true,
+        plugins: {
+          legend: {
+            position: "top",
+          },
+          title: {
+            display: true,
+            text: "Chart.js Line Chart",
+          },
+        },
+      },
+    });
+  }
+  showChart(secondTypeCoin.value);
+});
