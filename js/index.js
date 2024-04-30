0a080addf9931f59a260ae2c1525046efa10d64e
Ivanildo Queiroz <ivanildo.jua@hotmail.com>
2024-04-30 00:03:38 -0300

    update in chart and create of load

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const secondTypeCoin = document.querySelector("#toConversion");
-
+const secondTypeCoinGraph = secondTypeCoin.value
 secondTypeCoin.addEventListener("change", (e) => {
   e.preventDefault();
   const selectedValeu = document.querySelector("#selectedValue");
@@ -41,7 +41,6 @@ function conversionCoin(coin) {
   for (const key in coin) {
     calcConversion(coin[key].high);
   }
-
 }
 function getCoin(coin) {
   fetch(`https://economia.awesomeapi.com.br/last/${coin}-BRL`)
@@ -66,16 +65,23 @@ function calcConversion(VT) {
 }
 loadNameCoin();
 
-function insertClass() {
-  const timeList = document.querySelector("#tabList");
-  const timeListAll = timeList.querySelectorAll("li");
+async function insertClass() {
+  return new Promise((res, rej) => {
+    const timeList = document.querySelector("#tabList");
+    const timeListAll = timeList.querySelectorAll("li");
+    let valueDays;
 
-  timeListAll.forEach((e) => {
-    e.addEventListener("click", function () {
-      timeListAll.forEach((item) => {
-        if (item !== e) item.classList.remove("active");
+    timeListAll.forEach((e) => {
+      e.addEventListener("click", function () {
+        timeListAll.forEach((item) => {
+
+          if (item !== e) item.classList.remove("active");
+          e.classList.add("active");
+        });
+        // e.classList.toggle("active");
+        valueDays = e.value;
+        res(valueDays);
       });
-      e.classList.toggle("active");
     });
   });
 }
@@ -104,69 +110,105 @@ function alterDataDescription(x, y) {
     2
   )} reais`;
 }
+
 // chart
-secondTypeCoin.addEventListener("change", (e) => {
-  async function showChart(coin) {
-    async function getSpecificPeriod(x, y) {
-      let data, data_BRL;
-      let urlApi_BRL = `https://economia.awesomeapi.com.br/json/daily/BRL-${coin}/10?start_date=${x}&end_date=${y}`;
-      let urlApi = `https://economia.awesomeapi.com.br/json/daily/${coin}-BRL/10?start_date=${x}&end_date=${y}`;
-      try {
-        const res_BRL = await fetch(urlApi_BRL);
-        data_BRL = await res_BRL.json();
 
-        const res = await fetch(urlApi);
-        data = await res.json();
-        // console.log(data)
-        return { data, data_BRL };
-      } catch (e) {
-        console.log(e);
-        return null;
-      }
+async function showChart(coin) {
+  async function getSpecificPeriod() {
+    const valueDays = await insertClass();
+
+    let data, data_BRL;
+    let apiUrl_BRL = `https://economia.awesomeapi.com.br/json/daily/${coin}-BRL/${valueDays}`;
+    let apiUrl = `https://economia.awesomeapi.com.br/json/daily/BRL-${coin}/${valueDays}`;
+    // let urlApi_BRL = `https://economia.awesomeapi.com.br/json/daily/BRL-${coin}/10?start_date=${x}&end_date=${y}`;
+    // let urlApi = `https://economia.awesomeapi.com.br/json/daily/${coin}-BRL/10?start_date=${x}&end_date=${y}`;
+    try {
+      const res_BRL = await fetch(apiUrl_BRL);
+      data_BRL = await res_BRL.json();
+
+      const res = await fetch(apiUrl);
+      data = await res.json();
+      //const valueDays = await insertClass();
+
+      return { data, data_BRL, valueDays };
+    } catch (e) {
+      console.log(e);
+      return window.reload();
     }
-
-    //set initial data com 20240425 =  25/04/2024;
-    const dataApi = await getSpecificPeriod(20230102, 20230123);
-    const monthVariation_BRL = dataApi.data_BRL;
-    const monthVariation = dataApi.data;
-
-    const ctx = document.getElementById("chart");
-
-    new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: monthVariation.map((i) => calcTimeStamp(i.timestamp)),
-        datasets: [
-          {
-            label: `high ${coin}`,
-            data: monthVariation.map((i) => i.high),
-            borderWidth: 1,
-          },
-          {
-            label: "high BRL",
-            data: monthVariation_BRL.map((i) => i.high),
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: false,
-          },
-        },
-        responsive: true,
-        plugins: {
-          legend: {
-            position: "top",
-          },
-          title: {
-            display: true,
-            text: "Chart.js Line Chart",
-          },
-        },
-      },
-    });
   }
-  showChart(secondTypeCoin.value);
-});
+
+  //set initial data nno chart
+  const dataApi = await getSpecificPeriod();
+  const monthVariation_BRL = dataApi.data_BRL;
+  const monthVariation = dataApi.data;
+  dataLoad(false)
+  if (window.myChart) {
+    window.myChart.destroy();
+  }
+  const ctx = document.getElementById("chart");
+  
+  
+ const myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: monthVariation.map((i) => calcTimeStamp(i.timestamp)),
+      datasets: [
+        {
+          label: `high ${coin}`,
+          data: monthVariation.map((i) => i.high),
+          borderWidth: 1,
+        },
+        {
+          label: "high BRL",
+          data: monthVariation_BRL.map((i) => i.high),
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: false,
+        },
+      },
+      responsive: true,
+      plugins: {
+        legend: {
+          position: "",
+        },
+        title: {
+          display: true,
+          text: "Chart.js Line Chart",
+        },
+      },
+    },
+  });
+  window.myChart = myChart;
+}
+  secondTypeCoin.addEventListener("change", async (e) => {
+    await showChart(secondTypeCoin.value);
+    dataLoad(true)
+  });
+  
+  const timeList = document.querySelector("#tabList");
+  const timeListAll = timeList.querySelectorAll("li");
+  
+  timeListAll.forEach( (e) => {
+    e.addEventListener("click", function () {
+      timeListAll.forEach(async (item) => {
+          await showChart(secondTypeCoin.value);
+        });
+      });
+    });
+
+    function dataLoad(data){
+      if(data){
+
+        document.querySelector('.DisplayLoadData').classList.toggle('load-active');
+        document.querySelector('.DisplayLoadData').classList.toggle('load-desactive');
+    }
+    if(!data){
+        document.querySelector('.DisplayLoadData').classList.toggle('load-active');
+        document.querySelector('.DisplayLoadData').classList.toggle('load-desactive')
+    }
+  }
